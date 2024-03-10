026ae8e373a3d48827bc3106b21a992bb82ae443
Javier Flores <jffc.dev@gmail.com>
2024-03-09 21:29:14 -0500

    feat: CreateFormBtn styles modified

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -18,7 +18,9 @@ const Home = () => {
       <Separator className='my-6'></Separator>
       <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
       <Separator className='my-6'></Separator>
-      <CreateFormBtn></CreateFormBtn>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <CreateFormBtn/>
+      </div>
     </div>
   )
 }
--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -12,7 +12,6 @@ import {
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -55,7 +54,14 @@ const CreateFormBtn = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button>Create new form</Button>
+        <Button variant={'outline'} className='group border border-primary/20 h-[190px]
+        items-center justify-center flex flex-col hover:border-primary
+        hover:cursor-pointer border-dashed gap-4'>
+          <BsFileEarmarkPlus className='h-8 w-8 text-muted-foreground
+          group-hover:text-primary'/>
+          <p className='font-bold text-xl text-muted-foreground
+          group-hover:text-primary'>Create new form</p>
+        </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
