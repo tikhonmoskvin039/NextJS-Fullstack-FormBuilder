9e4488a37f8eeb9ee8bd55b58ca335c1876f7d09
Javier Flores <jffc.dev@gmail.com>
2024-03-13 01:28:59 -0500

    feat: form elements types added

---
+++ components/FormElements.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { TextFieldFormElement } from './fields/TextField'
+
+export type ElementsType = 'TextField'
+
+export type FormElementInstance = {
+    id: string
+    type: ElementsType
+    extraAttributes?: Record<string, any>
+}
+
+export type FormElement = {
+    type: ElementsType
+
+    construct: (id: string) => FormElementInstance
+
+    designerBtnElement: {
+        icon: React.ElementType,
+        label: string
+    }
+
+    designerComponent: React.FC<{
+        elementInstance: FormElementInstance
+    }>
+    formComponent: React.FC<{
+        elementInstance: FormElementInstance
+    }>
+    propertiesComponent: React.FC<{
+        elementInstance: FormElementInstance
+    }>
+}
+
+type FormElementsType = {
+    // eslint-disable-next-line no-unused-vars
+    [key in ElementsType]: FormElement
+}
+
+export const FormElements: FormElementsType = {
+  TextField: TextFieldFormElement
+}
