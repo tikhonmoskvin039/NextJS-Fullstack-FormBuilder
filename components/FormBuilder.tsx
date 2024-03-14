d32ae4d5fa8d0935c0d39612c09a7d7dd29b1958
Javier Flores <jffc.dev@gmail.com>
2024-03-14 01:30:40 -0500

    feat: react-confetti package installed

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -9,6 +9,12 @@ import { DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from '@dn
 import DragOverlayWrapper from './DragOverlayWrapper'
 import useDesigner from './hooks/useDesigner'
 import { ImSpinner2 } from 'react-icons/im'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { toast } from './ui/use-toast'
+import Link from 'next/link'
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
+import Confetti from 'react-confetti'
 
 const FormBuilder = ({ form }: {form: Form}) => {
   const { setElements } = useDesigner()
@@ -39,8 +45,53 @@ const FormBuilder = ({ form }: {form: Form}) => {
   if (!isReady) {
     return (
         <div className='flex flex-col items-center justify-center w-full h-full'>
-            <ImSpinner2 className='animate-spin h-12 w-12'/>
+          <ImSpinner2 className='animate-spin h-12 w-12'/>
         </div>
+    )
+  }
+
+  const shareUrl = `${window.location.origin}/submit/${form.shareUrl}`
+
+  if (form.published) {
+    return (
+      <>
+        <Confetti width={window.innerWidth} height={window.innerHeight} recycle={false} numberOfPieces={1000}/>
+        <div className='flex flex-col items-center justify-center w-full h-full'>
+          <div className='max-w-md'>
+            <h1 className='text-center text-4xl font-bold text-primary border-b pb-2 mb-10'>
+              🎉Form Published🎉
+            </h1>
+            <h2 className='text-2xl'>Share this form</h2>
+            <h3 className='texxt-xl text-muted-foreground border-b pb-10'>
+              Anyone with the link can view and submit the form
+            </h3>
+            <div className='my-4 flex flex-col gap-2 itmes-center w-full border-b pb-4'>
+              <Input className='w-full' readOnly value={shareUrl}></Input>
+              <Button className='mt-2 w-full' onClick={() => {
+                navigator.clipboard.writeText(shareUrl)
+                toast({
+                  title: 'Copied!',
+                  description: 'Link copied to clipboard'
+                })
+              }}>Copy Link</Button>
+            </div>
+            <div className='flex justify-between'>
+              <Button variant={'link'} asChild>
+                <Link href={'/'} className='gap-2'>
+                  <BsArrowLeft/>
+                  Go back home
+                </Link>
+              </Button>
+              <Button variant={'link'} asChild>
+                <Link href={`/forms/${form.id}`} className='gap-2'>
+                  Form details
+                  <BsArrowRight/>
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </>
     )
   }
 
