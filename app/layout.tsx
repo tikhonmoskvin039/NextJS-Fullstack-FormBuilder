24140dc598684800da7406b8a569060da6ba86d3
Javier Flores <jffc.dev@gmail.com>
2024-03-15 22:06:18 -0500

    feat: nextjs-toploader package installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/provider/ThemeProvider'
 import { ReactNode } from 'react'
 import { Toaster } from '@/components/ui/toaster'
 import DesignerContextProvider from '@/components/context/DesignerContext'
+import NextToploader from 'nextjs-toploader'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout ({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <NextToploader/>
           <DesignerContextProvider>
             <ThemeProvider
               attribute="class"
