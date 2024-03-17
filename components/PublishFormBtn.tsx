62615c25318f475eaa7b87ca10cf9d400d108399
Javier Flores <jffc.dev@gmail.com>
2024-03-17 16:32:11 -0500

    feat: PreviewDialogBtn, SaveFormBtn and PublishFormBtn updated

--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -1,13 +1,73 @@
-import React from 'react'
+import React, { useTransition } from 'react'
 import { Button } from './ui/button'
 import { MdOutlinePublish } from 'react-icons/md'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
 
-const PublishFormBtn = () => {
+} from './ui/alert-dialog'
+import { FaSpinner } from 'react-icons/fa'
+import { toast } from './ui/use-toast'
+import { PublishForm } from '@/actions/form'
+import { useRouter } from 'next/navigation'
+
+const PublishFormBtn = ({ id }:{id:number}) => {
+  const [loading, startTransition] = useTransition()
+  const router = useRouter()
+
+  const publishForm = async () => {
+    await PublishForm(id)
+    try {
+      toast({
+        title: 'Success',
+        description: 'Your form is now available to the public'
+      })
+      router.refresh()
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: 'Something went wrong'
+      })
+    }
+  }
+
   return (
-    <Button className='gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400'>
-        <MdOutlinePublish className='h-5 w-5'/>
-        Publish
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button className='gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400'>
+            <MdOutlinePublish className='h-5 w-5'/>
+            Publish
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>This action cannot be undone. After publishin you will not
+            be able to edit this form. <br/> <br/>
+            <span className='font-medium'>
+              By publishing this form you will make it available to the public and you will
+              be able to collect submissions.
+            </span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction disabled={loading} onClick={(e) => {
+            e.preventDefault()
+            startTransition(publishForm)
+          }}>Proceed {
+            loading && <FaSpinner className='animate-spin'/>
+          }</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
 
--- components/SaveFormBtn.tsx
+++ components/SaveFormBtn.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useTransition } from 'react'
 import { Button } from './ui/button'
 import { HiSaveAs } from 'react-icons/hi'
+import useDesigner from './hooks/useDesigner'
+import { toast } from './ui/use-toast'
+import { UpdateFormContent } from '@/actions/form'
+import { FaSpinner } from 'react-icons/fa'
 
-const SaveFormBtn = () => {
+const SaveFormBtn = ({ id }: {id: number}) => {
+  const { elements } = useDesigner()
+  const [loading, startTransition] = useTransition()
+
+  const updateFormContent = async () => {
+    try {
+      const JsonElements = JSON.stringify(elements)
+      await UpdateFormContent(id, JsonElements)
+      toast({
+        title: 'Success',
+        description: 'Your form has been saved'
+      })
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: 'Something went wrong.',
+        variant: 'destructive'
+      })
+    }
+  }
+
   return (
-    <Button className='gap-2' variant={'outline'}>
-        <HiSaveAs className='h-5 w-5'/>
-        Save
+    <Button className='gap-2' variant={'outline'} disabled={loading}
+    onClick={() => { startTransition(updateFormContent) }}>
+      <HiSaveAs className='h-5 w-5'/>
+      Save
+      {loading && <FaSpinner className='animate-spin'/>}
     </Button>
   )
 }
