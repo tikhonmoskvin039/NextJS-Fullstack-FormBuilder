49f28610771530d5d265bdb7c072481e4b018c4e
Javier Flores <jffc.dev@gmail.com>
2024-03-17 16:28:32 -0500

    feat: DesignerContext added

--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -2,8 +2,10 @@ import { Active, DragOverlay, useDndMonitor } from '@dnd-kit/core'
 import React, { useState } from 'react'
 import { SidebarBtnElementDragOverlay } from './SidebarBtnElement'
 import { ElementsType, FormElements } from './FormElements'
+import useDesigner from './hooks/useDesigner'
 
 const DragOverlayWrapper = () => {
+  const { elements } = useDesigner()
   const [draggedItem, setDraggedItem] = useState<Active | null>(null)
   useDndMonitor({
     onDragStart: (event) => {
@@ -25,6 +27,22 @@ const DragOverlayWrapper = () => {
   if (isSidebarBtnElement) {
     const type = draggedItem?.data?.current?.type as ElementsType
     node = <SidebarBtnElementDragOverlay formElement={FormElements[type]}/>
+  }
+
+  const isDesignerElement = draggedItem.data?.current?.isDesignerElement
+  if (isDesignerElement) {
+    const elementId = draggedItem.data?.current?.elementId
+    const element = elements.find((el) => {
+      return el.id === elementId
+    })
+    if (!element) node = <div>Element not found!</div>
+    else {
+      const DesignerElementComponent = FormElements[element.type].designerComponent
+      node = <div className='flex bg-accent border rounded-md h-[120px] w-full py-2 px-4
+        opacity-80 pointer-events-none'>
+        <DesignerElementComponent elementInstance={element}/>
+      </div>
+    }
   }
 
   return (
---
+++ components/context/DesignerContext.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { Dispatch, ReactNode, SetStateAction, createContext, useState } from 'react'
+import { FormElementInstance } from '../FormElements'
+
+type DesignerContextType = {
+    elements: FormElementInstance[]
+    setElements: Dispatch<SetStateAction<FormElementInstance[]>>
+    addElement: (index: number, element: FormElementInstance) => void
+    removeElement: (id: string) => void
+    updateElement: (id: string, element: FormElementInstance) => void
+
+    selectedElement: FormElementInstance | null
+    setSelectedElement: Dispatch<SetStateAction<FormElementInstance | null>>
+}
+
+export const DesignerContext = createContext<DesignerContextType | null>(null)
+
+const DesignerContextProvider = ({ children }:{children: ReactNode}) => {
+  const [elements, setElements] = useState<FormElementInstance[]>([])
+  const [selectedElement, setSelectedElement] = useState<FormElementInstance | null>(null)
+
+  const addElement = (index:number, element: FormElementInstance) => {
+    setElements((prev) => {
+      const newElements = [...prev]
+      newElements.splice(index, 0, element)
+      return newElements
+    })
+  }
+
+  const removeElement = (id: string) => {
+    setElements((prev) => {
+      return prev.filter((element) => element.id !== id)
+    })
+  }
+
+  const updateElement = (id:string, element: FormElementInstance) => {
+    setElements((prev) => {
+      const newElements = [...prev]
+      const index = newElements.findIndex(el => el.id === id)
+      newElements[index] = element
+      return newElements
+    })
+  }
+
+  return (
+    <DesignerContext.Provider value={{
+      elements,
+      setElements,
+      addElement,
+      removeElement,
+      selectedElement,
+      setSelectedElement,
+      updateElement
+    }}>
+        {children}
+    </DesignerContext.Provider>
+  )
+}
+
+export default DesignerContextProvider
