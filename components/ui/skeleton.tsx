79648f3d0938193141e929e448d3e0181c6abc3f
Javier Flores <jffc.dev@gmail.com>
2024-03-17 18:14:40 -0500

    fix: lint issues fixed

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,12 +1,13 @@
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
+import React from 'react'
 
-function Skeleton({
+function Skeleton ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      className={cn('animate-pulse rounded-md bg-primary/10', className)}
       {...props}
     />
   )
